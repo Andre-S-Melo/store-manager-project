05c357aa2149f9531ac61993548c0b58f70fdfa9
André Melo <adr.smelo@gmail.com>
2022-06-29 16:06:26 -0300

    feat: (error) implementa middlewares de erro

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const app = require('./app');
 require('dotenv').config();
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = require('./app');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
