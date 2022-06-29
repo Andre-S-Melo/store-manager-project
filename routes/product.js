fffa02073c2454c8099241dd6d61aedf15ba7cfa
André Melo <adr.smelo@gmail.com>
2022-06-29 18:43:49 -0300

    feat: (routes) implementa rota para o endpoint POST /products

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const products = require('../controllers/productControllers');
 const route = express.Router();
 
 route.get('/', products.getAll);
+route.post('/', products.create);
 route.get('/:id', products.getById);
 
 module.exports = route;
