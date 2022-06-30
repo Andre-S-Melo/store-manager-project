1788759e03058617a8cedd58c84fd225d1ebbb1e
André Melo <adr.smelo@gmail.com>
2022-06-30 17:43:46 -0300

    feat: (routes) implementa rotas para os endpoints /sales e /sales/:id

--- routes/sale.js
+++ routes/sale.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const sales = require('../controllers/saleControllers');
+const { validSale } = require('../middlewares/saleValidation');
 
 const route = express.Router();
 
-route.post('/', sales.create);
+route.get('/', sales.getAll);
+route.post('/', validSale, sales.create);
+route.get('/:id', sales.getById);
 
 module.exports = route;
