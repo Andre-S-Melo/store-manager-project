15d0d908a390f3710f2cfd7df38e99b493d95e10
André Melo <adr.smelo@gmail.com>
2022-07-01 12:04:11 -0300

    feat: (routes) implementa rota para o endpoint PUT /sales/:id

--- routes/sale.js
+++ routes/sale.js
@@ -7,6 +7,7 @@ const route = express.Router();
 route.get('/', sales.getAll);
 route.post('/', validSale, sales.create);
 route.get('/:id', sales.getById);
+route.put('/:id', sales.update);
 route.delete('/:id', sales.deleteById);
 
 module.exports = route;
